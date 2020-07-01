8b5f26f5e6177c63e9e9d7c3af8d8fd343936aa2
Nidhi Kumari <nidhi1691996@outlook.com>
2020-07-01 11:54:30 +0530

    app structure

---
+++ src/components/landingPage/landingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class landingPage extends Component {
+    render() { 
+        return ( 
+            <div>Hello</div>
+         );
+    }
+}
+ 
+export default landingPage;
---
+++ src/components/landingPage/nav.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class NavBar extends Component {
+    render() { 
+        return ( 
+            <div>Issues</div>
+         );
+    }
+}
+ 
+export default NavBar;
